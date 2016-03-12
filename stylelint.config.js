9750cc2d9a8366e2542029978d4692adefc23718
Adrian Hall <adrian@shellmonger.com>
2016-03-12 11:24:44 -0800

    Fixed tests for new results format in 4.5.x

---
+++ stylelint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+        'selector-list-comma-newline-after': [ 'always', { severity: 'warn' } ],
+        'unit-blacklist': [ 'px' ]
+    }
+}
---
+++ test/testfiles/test9/index.js
@@ -0,0 +1,3 @@
+require('./test9.scss');
+
+console.log('test9');
